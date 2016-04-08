477a8abe6a6caa5dc4a94e634390649d4e15680e
Samuli Tamminen <samuli.tamminen@gmail.com>
2016-04-08 17:18:45 +0300

    Updated thumbstick code

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,17 @@
 #include <Servo.h>
 
 
+
 void driveServoToNewPosition(int);
 bool changeServoAngle(int servo_num, int delta);
 int  angle(int);
-int  potValueToDelta(int);
+int  deltaForCtrl(int);
 void readCtrl();
 void printCtrlValues();
 void calibrateController();
+
+void handleThumbsticks();
+void handleL2R2();
 
 
 // Controller's potentiometers are connected to these pins
@@ -26,6 +30,8 @@ const int ctrl_right_X   = 0,
 int ctrl_values[8] = { 0 };
 int ctrl_pot_initials[6] = { 0 };
 
+const int ctrl_deadzone = 40; // plus-minus
+
 
 // Servo objects
 Servo parallax;
@@ -34,12 +40,12 @@ Servo mid;
 Servo grab_left;
 Servo grab_right;
 
-// Hold current angle of the servo
-// 0: parallax
-// 1: bottom
-// 2: mid
-// 3: grab_left
-// 4: grab_right
+const int servo_parallax   = 0,
+          servo_bottom     = 1,
+          servo_mid        = 2,
+          servo_grab_left  = 3,
+          servo_grab_rigth = 4;
+
 int servo_angles[5] = { 90, 90, 90, 90, 90 };
 
 
@@ -55,11 +61,29 @@ void setup() {
   grab_left.attach(6);
   grab_right.attach(7);
 
-  calibrateController();
 
   // for(int i = 0; i < 6; i++) {
   //   Serial.println(ctrl_pot_initials[i]);
   // }
+
+  // Wait for right button before continuing
+
+  int button;
+  bool pressed = false;
+
+  do {
+    bottom.write(90);
+    mid.write(90);
+    grab_left.write(90);
+    grab_right.write(90);
+    button = analogRead(ctrl_right_Btn);
+    pressed = (button < 100);
+    delay(15);
+  } while (!pressed);
+
+  calibrateController();
+
+  Serial.println("Program started!");
 
 }
 
@@ -69,78 +93,67 @@ void loop() {
   // Read controller's potentiometers to values-array
   readCtrl();
 
-  // Read each controller's potentiometer
-  for (int ctrl = 0; ctrl <= 7; ctrl++) {
+  handleThumbsticks();
+  handleL2R2();
 
-    switch (ctrl) {
-      case ctrl_right_X:
-      case ctrl_right_Y:
-      case ctrl_right_2:
-      case ctrl_left_X:
-      case ctrl_left_Y:
-      case ctrl_left_2:
-        driveServoToNewPosition(ctrl);
-        break;
+  //printCtrlValues();
 
-      default:
-        // For buttons, do nothing
-        break;
+  delay(50);
+}
 
-    }
 
+void handleThumbsticks() {
+  int delta;
+
+  // Parallax servo
+  // TODO
+
+  // Bottom servo
+  delta = deltaForCtrl(ctrl_right_Y);
+  // Serial.println(delta);
+  if (delta != 0 && changeServoAngle( servo_bottom, delta )) {
+    Serial.print("bottom to "); Serial.println(servo_angles[servo_bottom]);
+    bottom.write(servo_angles[ servo_bottom ]);
   }
 
-  printCtrlValues();
+  // Mid servo
+  delta = deltaForCtrl(ctrl_left_Y);
+  // Serial.println(delta);
+  if (delta != 0 && changeServoAngle( servo_mid, delta )) {
+    Serial.print("mid to "); Serial.println(servo_angles[servo_mid]);
+    mid.write(servo_angles[ servo_mid ]);
+  }
 
-  delay(100);
+  // Serial.println(" ");
+
+}
+
+void handleL2R2() {
+
 }
 
 
 
-void driveServoToNewPosition(int ctrl) {
-
-  int delta = potValueToDelta(ctrl_values[ctrl]);
-  if (delta == 0) {
-    return;
-  }
-
-  switch (ctrl) {
-    // case ctrl_right_X:
-    case ctrl_right_Y:
-      if (changeServoAngle(1, delta)) {
-        bottom.write(servo_angles[1]);
-        //Serial.print("bottom kulmaan "); Serial.print(servo_angles[1]); Serial.print("\n");
-      }
-      break;
-    // case ctrl_right_2:
-    // case ctrl_left_X:
-    case ctrl_left_Y:
-      if (changeServoAngle(2, delta)) {
-        mid.write(servo_angles[2]);
-        //Serial.print("mid kulmaan "); Serial.print(servo_angles[2]); Serial.print("\n");
-      }
-      break;
-    // case ctrl_left_2:
-    //   break;
-    default:
-      break;
-      // For buttons, do nothing
-  }
-
-}
-
+/**
+ * Change angle of a servo
+ * @param  servo_num The servo
+ * @param  delta     Amount of change
+ * @return           True if change succeeded
+ *                   False if servo is already on limit
+ */
 bool changeServoAngle(int servo_num, int delta) {
 
   int angle = servo_angles[servo_num];
 
-  if ((angle <= 1 && delta < 1) || (angle >= 176 && delta > -1)) {
+  // Check if servo is already on min or max angle
+  if ((angle <= 1 && delta < 1) || (angle >= 170 && delta > -1)) {
     return false;
   }
 
   if (angle + delta <= 1) {
     servo_angles[servo_num] = 1;
-  } else if (angle + delta >= 176) {
-    servo_angles[servo_num] = 176;
+  } else if (angle + delta >= 170) {
+    servo_angles[servo_num] = 170;
   } else {
     servo_angles[servo_num] += delta;
   }
@@ -156,38 +169,33 @@ int angle( int potvalue ) {
   return 90;
 }
 
-int potValueToDelta( int val ) {
-  // https://www.arduino.cc/en/Reference/Map
-  if (val < 400 || val > 600) {
-    return map(val, 0, 1023, -5, 5);
-  }
-  return 0;
+
+int deltaForCtrl( int ctrl ) {
+
+  int value = ctrl_values[ctrl];
+  int initial = ctrl_pot_initials[ctrl];
+
+  if (value < initial - ctrl_deadzone) {
+     return map(value, 0, initial - ctrl_deadzone, -5, 0);
+  } else if (value > initial + ctrl_deadzone) {
+    return map(value, initial + ctrl_deadzone, 1023, 0, 5);
+  } else {
+     return 0;
+   }
 }
 
 
 // Read controller's potentiometers
 void readCtrl() {
-  int deadzone = 40; // plus-minus
   int reading = 0;
 
-  // Thumbsticks
-  for (int ctrl = 0; ctrl <= 3; ctrl++) {
-    // Snap to center (deadzone)
-    reading = analogRead(ctrl);
-    if (reading < ctrl_pot_initials[ctrl] - deadzone || reading > ctrl_pot_initials[ctrl] + deadzone) {
-      ctrl_values[ctrl] = reading;
-    } else {
-      ctrl_values[ctrl] = 500;
-    }
-  }
-
-  // L2 and R2
-  for (int ctrl = 4; ctrl <= 5; ctrl++) {
+  // Thumbsticks, L2 and R2
+  for (int ctrl = 0; ctrl <= 5; ctrl++) {
     reading = analogRead(ctrl);
     ctrl_values[ctrl] = reading;
   }
 
-  // Thumbstick buttons
+  // Thumbstick buttons -> 0/1
   for (int ctrl = 6; ctrl <= 7; ctrl++) {
     reading = analogRead(ctrl);
     // ctrl_values[ctrl] = reading;
